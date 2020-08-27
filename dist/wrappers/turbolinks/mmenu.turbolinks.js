25dcb8d2662b0a441979b3c1163669f8d615bca4
asimsikka <KhawajaAsim.Sikka@gmail.com>
2020-08-27 21:57:28 +0500

    Update mmenu.turbolinks.js

--- dist/wrappers/turbolinks/mmenu.turbolinks.js
+++ dist/wrappers/turbolinks/mmenu.turbolinks.js
@@ -1,6 +1,7 @@
 export default function () {
     var classnames;
     document.addEventListener('turbolinks:request-start', function (evnt) {
+        console.log("request-start");
         classnames = document
             .querySelector('.mm-wrapper')
             .className.split(' ')
